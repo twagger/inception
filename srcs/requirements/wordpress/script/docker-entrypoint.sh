6a9633270e11b2c1ee3901cdd881699f71789a62
twagner <twagner@student.42.fr>
2022-08-11 17:38:55 +0200

    fix: I forgot to add another user in wordpress database

--- srcs/requirements/wordpress/script/docker-entrypoint.sh
+++ srcs/requirements/wordpress/script/docker-entrypoint.sh
@@ -10,8 +10,8 @@ chmod +x wp-cli.phar
 # configure wp
 ./wp-cli.phar config create \
     --dbname=$WP_DB_NAME \
-    --dbuser=$WP_DB_USER \
-    --dbpass=$WP_DB_PASSWORD \
+    --dbuser=$WP_DB_ADM_USER \
+    --dbpass=$WP_DB_ADM_PASSWORD \
     --dbhost=$WP_DB_HOST \
     --dbprefix=$WP_TABLE_PREFIX \
     --dbcharset=$WP_DB_CHARSET \
@@ -24,10 +24,15 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core install \
     --url=$WP_SITE_URL \
     --title=$WP_SITE_TITLE \
-    --admin_user=$WP_DB_USER \
-    --admin_password=$WP_DB_PASSWORD \
+    --admin_user=$WP_DB_ADM_USER \
+    --admin_password=$WP_DB_ADM_PASSWORD \
     --admin_email=$WP_ADMIN_EMAIL \
     --skip-email
+
+# add a non root user
+eval "echo \"$(cat /tmp/mysql/wp_db_user.sql)\"" > /tmp/mysql/import.sql
+mysql -h mariadb -u $WP_DB_ADM_USER --password=$WP_DB_ADM_PASSWORD < "/tmp/mysql/import.sql"
+rm -rf /tmp/mysql/db_config.sql /tmp/mysql/import.sql
 
 # add a theme
 ./wp-cli.phar theme install twentytwenty --activate
